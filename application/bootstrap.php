e6603722591773b78af58c0f0b3c169ac4cda184
Sopa-de-Ideias <sopadeideias@gmail.com>
2016-12-17 03:16:39 -0200

    Revert "Creation of Routes File"
    This reverts commit c381b36b243fc237a9b7c613d08e3e80fa9d698d.

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -140,4 +140,12 @@ Kohana::modules(array(
  */
 // Cookie::$salt = NULL;
 
-require APPPATH.'routes'.EXT;
+/**
+ * Set the routes. Each route must have a minimum of a name, a URI and a set of
+ * defaults for the URI.
+ */
+Route::set('default', '(<controller>(/<action>(/<id>)))')
+	->defaults(array(
+		'controller' => 'welcome',
+		'action'     => 'index',
+	));
